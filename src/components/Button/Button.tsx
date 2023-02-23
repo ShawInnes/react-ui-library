699c9fb353c8c3973a2286ac236d1c56fc1b891a
Shaw Innes <shaw@heylemonade.com.au>
2023-02-23 20:16:18 +1000

    Tweak stories

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import "./Button.scss";
+import React, {forwardRef} from 'react';
+import './Button.scss';
 
 export interface ButtonProps {
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
+const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
   return <button>{props.label}</button>;
 };
 
 export default Button;
+
