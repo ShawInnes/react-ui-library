4d45014af1bc3418a7a03a918e72c01e0f3894b4
Shaw Innes <shaw@heylemonade.com.au>
2023-02-23 19:28:42 +1000

    Add CSS Support

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-import postcss from "rollup-plugin-postcss";
+import postcss from 'rollup-plugin-postcss'
+
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
@@ -17,11 +18,13 @@ export default [
                 file: packageJson.main,
                 format: "cjs",
                 sourcemap: true,
+                assetFileNames: '[name][extname]'
             },
             {
                 file: packageJson.module,
                 format: "esm",
                 sourcemap: true,
+                assetFileNames: '[name][extname]'
             },
         ],
         plugins: [
@@ -29,14 +32,18 @@ export default [
             resolve(),
             commonjs(),
             typescript({tsconfig: "./tsconfig.json"}),
+
             postcss(),
+
             terser(),
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{file: "dist/index.d.ts", format: "esm"}],
-        plugins: [dts()],
-        external: [/\.css$/],
+        plugins: [
+            dts(),
+        ],
+        external: [/\.css/,/\.scss/]
     },
 ];
